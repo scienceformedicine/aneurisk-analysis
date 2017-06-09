7f86bcb685ac7766686434a73addd27ab60496c0
moyano <moyano@luiss-MBP.lan>
2017-06-09 02:12:09 -0300

    remove space from dirs 2

--- SOURCE - S4M/4-aneurisk-figs.R
+++
@@ -1,5 +0,0 @@
-## aneurisk figures - Fri 06/02/17 11:01:39
-
-## boxplot of tortuosity for different MORPHO_SHAPES
-ggplot(aneurisk) + geom_boxplot(aes(x = as.factor(MORPHO_SHAPE), y = tortuosity), alpha = 0.3) 
-
